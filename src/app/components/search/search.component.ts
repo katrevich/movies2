448a679f5d384ab374abeb41be04f79958191d95
katrevich <skatrevich@gmail.com>
2017-04-27 16:55:58 +0300

    package fix

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent{
+export class SearchComponent {
 
   constructor() { }
 }
